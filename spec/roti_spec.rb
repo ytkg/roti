868a5a2cffa0ef01c3d636867470811df75258d1
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-07-30 18:53:50 +0900

    initial commit

---
+++ spec/roti_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Roti do
+  it "has a version number" do
+    expect(Roti::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
