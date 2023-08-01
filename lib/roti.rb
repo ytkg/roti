58effd53d83ae50018f9753176b70d77362e728b
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-08-02 01:06:11 +0900

    Implement ROTI encoding and decoding

--- lib/roti.rb
+++ lib/roti.rb
@@ -4,5 +4,31 @@ require_relative "roti/version"
 
 module Roti
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    CHARACTOR_LIST = ("!".."~").to_a
+    CHARACTOR_LIST_SIZE = CHARACTOR_LIST.size
+
+    def encode(string)
+      string.chars.map.with_index(1) { |charactor, index| enecode_charactor(charactor, index) }.join
+    end
+
+    def decode(string)
+      string.chars.map.with_index(1) { |charactor, index| decode_charactor(charactor, index) }.join
+    end
+
+    private
+
+    def enecode_charactor(charactor, index)
+      return charactor unless CHARACTOR_LIST.include?(charactor)
+
+      CHARACTOR_LIST[(CHARACTOR_LIST.index(charactor) + index) % CHARACTOR_LIST_SIZE]
+    end
+
+    def decode_charactor(charactor, index)
+      return charactor unless CHARACTOR_LIST.include?(charactor)
+
+      CHARACTOR_LIST[(CHARACTOR_LIST.index(charactor) - index) % CHARACTOR_LIST_SIZE]
+    end
+  end
 end
--- spec/roti_spec.rb
+++ spec/roti_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Roti do
   it "has a version number" do
     expect(Roti::VERSION).not_to be nil
   end
+
+  it { expect(Roti.encode("Hello, world!")).to eq "Igopt2 !x|wp." }
+  it { expect(Roti.decode("Igopt2 !x|wp.")).to eq "Hello, world!" }
 end
